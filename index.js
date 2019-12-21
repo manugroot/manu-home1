2a4fb8f3a57d36819fc18b0697d5d296f884589f
manugroot <manishreddy9963@gmail.com>
2019-12-21 22:08:14 +0530

    home project

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT||3000
 app.get('/', (req, res) => res.sendFile(__dirname + '/public/htmls/index.html'))
 
 var obj={}
@@ -8,3 +12,10 @@ obj.regno = "111111ABC";
 app.get('/data', (req, res) => res.json(obj))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
+
+
+
+
+
